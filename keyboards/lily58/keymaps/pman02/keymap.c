b2831aab508fde9b56f3bf5e85316f7cf64da937
Patrick Wright <patrick@yellow-bridge.com>
2021-01-10 17:06:41 -0500

    swapped return and space

--- keyboards/lily58/keymaps/pman02/keymap.c
+++ keyboards/lily58/keymaps/pman02/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, KC_ENT, MO(_LOWER), MO(_RAISE),KC_SPC , KC_BSPC, KC_DEL \
+                        KC_LALT, KC_LGUI, MO(_LOWER) , KC_ENT , KC_SPC,MO(_RAISE) , KC_BSPC, KC_DEL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
+ * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   ~  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -133,24 +133,24 @@ void set_keylog(uint16_t keycode, keyrecord_t *record);
 const char *read_keylog(void);
 const char *read_keylogs(void);
 
- const char *read_mode_icon(bool swap);
- const char *read_host_led_state(void);
- void set_timelog(void);
- const char *read_timelog(void);
+//  const char *read_mode_icon(bool swap);
+//  const char *read_host_led_state(void);
+//  void set_timelog(void);
+//  const char *read_timelog(void);
 
 void oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
-     oled_write_P(PSTR("Layer: "), false);
+//     oled_write_P(PSTR("Layer: "), false);
 
     oled_write_ln(read_layer_state(), false);
     oled_write_ln(read_keylog(), false);
     oled_write_ln(read_keylogs(), false);
-    oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    oled_write_ln(read_host_led_state(), false);
-    oled_write_ln(read_timelog(), false);
+    // oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    // oled_write_ln(read_host_led_state(), false);
+    // oled_write_ln(read_timelog(), false);
   } else {
-    oled_write_P(PSTR("Layer: "), false);
+ //   oled_write_P(PSTR("Layer: "), false);
     oled_write(read_logo(), false);
   }
 }
@@ -161,7 +161,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef OLED_DRIVER_ENABLE
     set_keylog(keycode, record);
 #endif
-     set_timelog();
+   //  set_timelog();
   }
   return true;
 }
